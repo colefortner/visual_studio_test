070f6f53190d3a709b3b0b5fc16d4dc4d6da3e65
colefortner <colefortner@gmail.com>
2024-02-10 20:50:47 -0600

    Initial commit from visual studio

---
+++ Old_School/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Old_School;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        string myname = "Cole";
+
+        string message = "My name is " + myname;
+
+        string capsMessage = message.ToLower();
+
+        Console.WriteLine(message);
+        Console.WriteLine(capsMessage);
+    }
+}
+
